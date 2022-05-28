81f2454069d475b2ea22522af2042bf5044471cb
Berke <hello@berke.dev>
2022-05-28 17:15:12 +0300

    fix restcountries api url

--- scripts.js
+++ scripts.js
@@ -62,7 +62,7 @@ const search = (q) => {
 const getSearchedDetails = (index) => {
   const country = countries[index];
   fetch(
-    `https://restcountries.com/v2/alpha/${country.CountryCode}?fields=flag;population`
+    `https://restcountries.com/v2/alpha/${country.CountryCode}?fields=flag,population`
   )
     .then((response) => response.json())
     .then((data) => {
